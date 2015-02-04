e738ab8553de97954f13988a32f5ad1b214713ac
Hujie Wang <wanghujie@gmail.com>
2015-02-03 23:10:16 -0500

    headers

---
+++ notebook/Headers/common.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define mt make_tuple
+#define mp make_pair
+
+using namespace std;
+typedef tuple<int,int,int> TIII;
+typedef tuple<int,int> TII;
+typedef pair<int,int> PII;
+typedef long long LL;
