6939fecfbf027385e3b2384ccb841fc5c5b3d1fe
Hujie Wang <wanghujie@gmail.com>
2015-02-28 20:37:54 -0500

    Updated headers

--- notebook/Headers/common.cpp
+++ notebook/Headers/common.cpp
@@ -3,7 +3,12 @@
 #define mp make_pair
 
 using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORR(i,a,b) for(int i=(b);i>=(a);i--)
+#define F(i,n) for(int i=0;i<(n);i++)
+#define FR(i,n) for(int i=(n)-1;i>=0;i--)
 typedef tuple<int,int,int> TIII;
 typedef tuple<int,int> TII;
 typedef pair<int,int> PII;
 typedef long long LL;
+
