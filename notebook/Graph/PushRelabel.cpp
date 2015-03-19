32b2054ecc8852ed34965bf551981fb7ae644496
Hujie Wang <wanghujie@gmail.com>
2015-03-19 18:11:59 -0400

    Added get_min_cut into PushRelabel.cpp

--- notebook/Graph/PushRelabel.cpp
+++ notebook/Graph/PushRelabel.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define pb push_back
+#define mt make_tuple
+#define mp make_pair
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORR(i,a,b) for(int i=(b);i>=(a);i--)
+#define F(i,n) for(int i=0;i<(n);i++)
+#define FR(i,n) for(int i=(n)-1;i>=0;i--)
+#define R(a,i,b) (a<=i&&i<=b)
+typedef tuple<int,int,int> TIII;
+typedef tuple<int,int> TII;
+typedef pair<int,int> PII;
 typedef long long LL;
+
 // Adjacency list implementation of FIFO push relabel maximum flow
 // with the gap relabeling heuristic.  This implementation is
 // significantly faster than straight Ford-Fulkerson.  It solves
@@ -20,91 +33,111 @@ typedef long long LL;
 //     - maximum flow value
 //     - To obtain the actual flow values, look at all edges with
 //       capacity > 0 (zero capacity edges are residual edges).
-
-
+//
+// Residual Graph: e.flow!=e.cap
 struct Edge {
-  int from, to, cap, flow, index;
-  Edge(int from, int to, int cap, int flow, int index) :
-    from(from), to(to), cap(cap), flow(flow), index(index) {}
+   int from, to, cap, flow, index;
+   Edge(int from, int to, int cap, int flow, int index) :
+      from(from), to(to), cap(cap), flow(flow), index(index) {}
 };
-struct PushRelabel {
-  int N;
-  vector<vector<Edge> > G;
-  vector<LL> excess;
-  vector<int> dist, active, count;
-  queue<int> Q;
 
-  PushRelabel(int N) : N(N), G(N), excess(N), dist(N), active(N), count(2*N) {}
+struct MaxFlow {
+   int N;
+   vector<vector<Edge> > G;
+   vector<LL> excess;
+   vector<int> dist, active, count;
+   queue<int> Q;
 
-  void AddEdge(int from, int to, int cap) {
-    G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
-    if (from == to) G[from].back().index++;
-    G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
-  }
+   MaxFlow(int N) : N(N), G(N), excess(N), dist(N), active(N), count(2*N) {}
 
-  void Enqueue(int v) { 
-    if (!active[v] && excess[v] > 0) { active[v] = true; Q.push(v); } 
-  }
+   void add_edge(int from, int to, int cap) {
+      G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+      if (from == to) G[from].back().index++;
+      G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
+   }
 
-  void Push(Edge &e) {
-    int amt = int(min(excess[e.from], LL(e.cap - e.flow)));
-    if (dist[e.from] <= dist[e.to] || amt == 0) return;
-    e.flow += amt;
-    G[e.to][e.index].flow -= amt;
-    excess[e.to] += amt;    
-    excess[e.from] -= amt;
-    Enqueue(e.to);
-  }
+   void enqueue(int v) { 
+      if (!active[v] && excess[v] > 0) { active[v] = true; Q.push(v); } 
+   }
+
+   void push(Edge &e) {
+      int amt = int(min(excess[e.from], LL(e.cap - e.flow)));
+      if (dist[e.from] <= dist[e.to] || amt == 0) return;
+      e.flow += amt;
+      G[e.to][e.index].flow -= amt;
+      excess[e.to] += amt;    
+      excess[e.from] -= amt;
+      enqueue(e.to);
+   }
   
-  void Gap(int k) {
-    for (int v = 0; v < N; v++) {
-      if (dist[v] < k) continue;
+   void gap(int k) {
+      for (int v = 0; v < N; v++) {
+	 if (dist[v] < k) continue;
+	 count[dist[v]]--;
+	 dist[v] = max(dist[v], N+1);
+	 count[dist[v]]++;
+	 enqueue(v);
+      }
+   }
+
+   void relabel(int v) {
       count[dist[v]]--;
-      dist[v] = max(dist[v], N+1);
+      dist[v] = 2*N;
+      for (int i = 0; i < G[v].size(); i++) 
+	 if (G[v][i].cap - G[v][i].flow > 0)
+	    dist[v] = min(dist[v], dist[G[v][i].to] + 1);
       count[dist[v]]++;
-      Enqueue(v);
-    }
-  }
+      enqueue(v);
+   }
 
-  void Relabel(int v) {
-    count[dist[v]]--;
-    dist[v] = 2*N;
-    for (int i = 0; i < G[v].size(); i++) 
-      if (G[v][i].cap - G[v][i].flow > 0)
-	dist[v] = min(dist[v], dist[G[v][i].to] + 1);
-    count[dist[v]]++;
-    Enqueue(v);
-  }
+   void discharge(int v) {
+      for (int i = 0; excess[v] > 0 && i < G[v].size(); i++) push(G[v][i]);
+      if (excess[v] > 0) {
+	 if (count[dist[v]] == 1) 
+	    gap(dist[v]); 
+	 else
+	    relabel(v);
+      }
+   }
 
-  void Discharge(int v) {
-    for (int i = 0; excess[v] > 0 && i < G[v].size(); i++) Push(G[v][i]);
-    if (excess[v] > 0) {
-      if (count[dist[v]] == 1) 
-	Gap(dist[v]); 
-      else
-	Relabel(v);
-    }
-  }
-
-  LL GetMaxFlow(int s, int t) {
-    count[0] = N-1;
-    count[N] = 1;
-    dist[s] = N;
-    active[s] = active[t] = true;
-    for (int i = 0; i < G[s].size(); i++) {
-      excess[s] += G[s][i].cap;
-      Push(G[s][i]);
-    }
+   LL get_max_flow(int s, int t) {
+      count[0] = N-1;
+      count[N] = 1;
+      dist[s] = N;
+      active[s] = active[t] = true;
+      for (int i = 0; i < G[s].size(); i++) {
+	 excess[s] += G[s][i].cap;
+	 push(G[s][i]);
+      }
     
-    while (!Q.empty()) {
-      int v = Q.front();
-      Q.pop();
-      active[v] = false;
-      Discharge(v);
-    }
+      while (!Q.empty()) {
+	 int v = Q.front();
+	 Q.pop();
+	 active[v] = false;
+	 discharge(v);
+      }
     
-    LL totflow = 0;
-    for (int i = 0; i < G[s].size(); i++) totflow += G[s][i].flow;
-    return totflow;
-  }
+      LL totflow = 0;
+      for (int i = 0; i < G[s].size(); i++) totflow += G[s][i].flow;
+      return totflow;
+   }
+
+   void dfs_residual(int u,vector<int> &min_cut,vector<bool> &vis){
+      min_cut.push_back(u);
+      vis[u]=true;
+      for(auto e:G[u]){
+	 if((e.flow!=e.cap)&&!vis[e.to]){
+	    dfs_residual(e.to,min_cut,vis);
+	 }
+      }
+   }
+   /*
+     Return the partition that contains s (a.k.a source)
+   */
+   vector<int> get_min_cut_partition(int s){
+      vector<int> min_cut;
+      vector<bool> vis(N,false);
+      dfs_residual(s,min_cut,vis);
+      return min_cut;
+   }
 };
