953fb7fa0976f90f63d23ecab6ecb6e69886b585
Hujie Wang <wanghujie@gmail.com>
2015-12-06 22:38:55 -0500

    morrisTraversal and bst serialization

---
+++ notebook/BST Alg/morrisTraversal.cpp
@@ -0,0 +1,28 @@
+/*
+ Inorder Traversal in O(n) with space O(1)
+ */
+void MorrisTraversal(TreeNode *root){
+  while(root){
+    cout<<root->val<<endl;
+    if(root->left){
+      TreeNode *pred=root->left;
+      while(pred->right&&pred->right!=root)pred=pred->right;
+            
+      if(pred->right==root){
+	pred->right=NULL;
+	// Do something with root
+	print(root->val);
+	root=root->right;
+      }
+      else{
+	pred->right=root;
+	root=root->left;
+      }
+    }
+    else{
+      // Do something with root
+      print(root->val);
+      root=root->right;
+    }
+  }
+}
